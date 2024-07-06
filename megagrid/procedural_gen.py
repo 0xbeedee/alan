92e6665e6c3ea428a274c0064e3bedc6c3583fc7
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-07-06 18:11:01 +0200

    initial megagrid environment code

--- megagrid/procedural_gen.py
+++ megagrid/procedural_gen.py
@@ -1,12 +1,14 @@
 import random
 
 import gymnasium as gym
+from megagrid.utils import get_possible_minigrids
 
 
 def generate_grid():
     return 0
 
 
-def random_gen(grid_options):
+def random_gen(seed=None):
     """The simplest possible procedural generation: return environments at random."""
+    grid_options = get_possible_minigrids()
     return gym.make(random.choice(grid_options))

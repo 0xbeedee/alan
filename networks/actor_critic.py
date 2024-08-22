491fcdf7a7a329d21af399ca994b2134756db996
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-22 17:46:05 +0200

    changed API for better compatibility with tianshou

--- networks/actor_critic.py
+++ networks/actor_critic.py
@@ -1,38 +1,47 @@
 from torch import nn
 
-from .observation_net import NetHackObsNet
 
-
-class SimpleNetHackActor(NetHackObsNet):
+class SimpleNetHackActor(nn.Module):
     def __init__(
         self,
+        obs_net,
         observation_space,
         action_space,
         embedding_dim=32,
         crop_dim=9,
         num_layers=5,
     ):
-        super().__init__(observation_space, embedding_dim, crop_dim, num_layers)
-        self.n_actions = action_space.n
+        super().__init__()
 
-        self.fc = nn.Sequential(self.fc, nn.Linear(self.h_dim // 2, self.n_actions))
+        self.obs_net = obs_net
+        self.obs_net.__init__(observation_space, embedding_dim, crop_dim, num_layers)
+
+        self.n_actions = action_space.n
+        self.final_layer = nn.Linear(self.obs_net.o_dim, self.n_actions)
 
     def forward(self, batch_obs, state=None, info={}):
-        logits = super().forward(batch_obs)
+        obs_out = self.obs_net.forward(batch_obs)
+        logits = self.final_layer(obs_out)
         return logits, state
 
 
-class SimpleNetHackCritic(NetHackObsNet):
+class SimpleNetHackCritic(nn.Module):
     def __init__(
         self,
+        obs_net,
         observation_space,
         embedding_dim=32,
         crop_dim=9,
         num_layers=5,
     ):
-        super().__init__(observation_space, embedding_dim, crop_dim, num_layers)
+        super().__init__()
 
-        self.fc = nn.Sequential(self.fc, nn.Linear(self.h_dim // 2, 1))
+        self.obs_net = obs_net
+        self.obs_net.__init__(observation_space, embedding_dim, crop_dim, num_layers)
+
+        self.final_layer = nn.Linear(self.obs_net.o_dim, 1)
 
     def forward(self, batch_obs, state=None, info={}):
-        return super().forward(batch_obs)
+        obs_out = self.obs_net.forward(batch_obs)
+        v_s = self.final_layer(obs_out)
+        return v_s

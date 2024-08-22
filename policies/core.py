86ccc1df1edd78603b299099eacfea6895d78e97
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-22 18:29:21 +0200

    minor edits

--- policies/core.py
+++ policies/core.py
@@ -4,7 +4,7 @@ from abc import abstractmethod
 
 import gymnasium as gym
 
-from numpy import ndarray
+import numpy as np
 from tianshou.data import ReplayBuffer
 from tianshou.data.types import BatchWithReturnsProtocol, RolloutBatchProtocol
 from tianshou.policy import BasePolicy
@@ -27,13 +27,13 @@ class CorePolicy(BasePolicy[CoreTrainingStats]):
     def __init__(
         self,
         *,
+        self_model: SelfModel,
+        env_model: EnvModel,
         action_space: gym.Space,
-        observation_space: gym.Space | None = None,
+        observation_space: gym.Space | None,
         action_scaling: bool = False,
         action_bound_method: None | Literal["clip"] | Literal["tanh"] = "clip",
         lr_scheduler: TLearningRateScheduler | None = None,
-        self_model: SelfModel,
-        env_model: EnvModel
     ) -> None:
         super().__init__(
             action_space=action_space,
@@ -47,7 +47,7 @@ class CorePolicy(BasePolicy[CoreTrainingStats]):
         self.env_model = env_model
 
     @abstractmethod
-    def _combine_reward(self, batch: RolloutBatchProtocol) -> float:
+    def combine_reward(self, batch: RolloutBatchProtocol) -> np.ndarray:
         """Combines the intrinsic and extrinsic rewards into a single scalar value."""
 
     @classmethod
@@ -55,16 +55,15 @@ class CorePolicy(BasePolicy[CoreTrainingStats]):
         cls,
         batch: RolloutBatchProtocol,
         buffer: ReplayBuffer,
-        indices: ndarray,
-        v_s_: ndarray | Tensor | None = None,
-        v_s: ndarray | Tensor | None = None,
+        indices: np.ndarray,
+        v_s_: np.ndarray | Tensor | None = None,
+        v_s: np.ndarray | Tensor | None = None,
         gamma: float = 0.99,
         gae_lambda: float = 0.95,
-    ) -> tuple[ndarray, ndarray]:
+    ) -> tuple[np.ndarray, np.ndarray]:
         # by default, batch.rew contains the reward provided by the env
-        batch.rew = cls._combine_reward(batch)
-
-        return super().compute_episodic_return(
+        batch.rew = cls.combine_reward(batch)
+        return BasePolicy.compute_episodic_return(
             batch, buffer, indices, v_s_, v_s, gamma, gae_lambda
         )
 
@@ -73,15 +72,14 @@ class CorePolicy(BasePolicy[CoreTrainingStats]):
         cls,
         batch: RolloutBatchProtocol,
         buffer: ReplayBuffer,
-        indices: ndarray,
-        target_q_fn: Callable[[ReplayBuffer, ndarray], Tensor],
+        indices: np.ndarray,
+        target_q_fn: Callable[[ReplayBuffer, np.ndarray], Tensor],
         gamma: float = 0.99,
         n_step: int = 1,
         rew_norm: bool = False,
     ) -> BatchWithReturnsProtocol:
         # by default, batch.rew contains the reward provided by the env
-        batch.rew = cls._combine_reward(batch)
-
-        return super().compute_nstep_return(
+        batch.rew = cls.combine_reward(batch)
+        return BasePolicy.compute_nstep_return(
             batch, buffer, indices, target_q_fn, gamma, n_step, rew_norm
         )

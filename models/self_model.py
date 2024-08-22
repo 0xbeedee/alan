b6632b1c6e87b2bf8c6591c2cc1f71a01ea31137
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-22 18:29:07 +0200

    simplified the code

--- models/self_model.py
+++ models/self_model.py
@@ -1,22 +1,8 @@
-from intrinsic.icm import ICM
-
-
 class SelfModel:
-    def __init__(self, obs_net, action_space) -> None:
-        self.intrinsic_module = ICM(obs_net, action_space.n)
+    def __init__(self, obs_net, action_space, intrinsic_module) -> None:
+        self.intrinsic_module = intrinsic_module(obs_net, action_space.n)
 
-    def __call__(self, obs_batch, act_batch, obs_prime_batch, sleep=False):
-        i_reward, _ = self.intrinsic_module.forward(
-            obs_batch, act_batch, obs_prime_batch
-        )
+    def __call__(self, batch, sleep=False):
+        i_reward, _ = self.intrinsic_module.forward(batch)
 
-        if sleep:
-            self._sleep()
-
-        # the policy will take this reward an extrinsic one as input and decide how to act
         return i_reward
-
-    # def _sleep(self):
-    #     aggregate_experience()
-    #     build_hierarchies()
-    #     add_new_knowledge_to_KB()

069b6bc5108df6cede8faac5aee3fbd10f2f8789
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-23 20:04:09 +0200

    simplified code

--- models/self_model.py
+++ models/self_model.py
@@ -3,6 +3,6 @@ class SelfModel:
         self.intrinsic_module = intrinsic_module(obs_net, action_space.n)
 
     def __call__(self, batch, sleep=False):
-        i_reward, _ = self.intrinsic_module.forward(batch)
+        i_reward = self.intrinsic_module.forward(batch)
 
         return i_reward

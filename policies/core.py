a5efb24beac4ca281723e5e8d07b2f899b62fdce
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-08-25 16:42:46 +0200

    fixed an error with reward combination and added various touchups

--- policies/core.py
+++ policies/core.py
@@ -1,6 +1,8 @@
 from typing import Literal
-import gymnasium as gym
+
+from gymnasium import Space
 import numpy as np
+
 from tianshou.data import ReplayBuffer
 from tianshou.data.types import (
     RolloutBatchProtocol,
@@ -15,18 +17,23 @@ from models import SelfModel, EnvModel
 
 class CoreTrainingStats(TrainingStatsWrapper):
     def __init__(self, wrapped_stats):
-        # TODO should I add more to this?
         super().__init__(wrapped_stats)
+        # TODO should I add more to this?
 
 
 class CorePolicy(BasePolicy[CoreTrainingStats]):
+    """This is the Core policy object we'll use throughout.
+
+    It is analogous to Tianshou's BasePolicy, in that each policy we create MUST inherit from it.
+    """
+
     def __init__(
         self,
         *,
         self_model: SelfModel,
         env_model: EnvModel,
-        action_space: gym.Space,
-        observation_space: gym.Space | None,
+        action_space: Space,
+        observation_space: Space | None,
         action_scaling: bool = False,
         action_bound_method: None | Literal["clip"] | Literal["tanh"] = "clip",
         lr_scheduler: TLearningRateScheduler | None = None,
@@ -49,22 +56,21 @@ class CorePolicy(BasePolicy[CoreTrainingStats]):
 
     @beta.setter
     def beta(self, value: float) -> None:
+        if value <= 0:
+            raise ValueError("The beta parameter must be greater than zero!")
         self._beta = value
 
     def get_beta(self) -> float:
-        """
-        Override this method in subclasses to implement custom beta calculation logic.
-        """
+        """Override this method in subclasses to implement custom beta calculation logic."""
         return self.beta
 
     def combine_reward(self, batch: RolloutBatchProtocol) -> np.ndarray:
-        """Combines the intrinsic and extrinsic rewards into a single scalar value.
+        """Combines the intrinsic and extrinsic rewards into a single scalar value in-place.
 
-        We provide a default implementation, but this method is meant to be overridden.
+        A default implementation is provided, but this method is meant to be overridden.
         """
-        print("in combien reward!!")
         i_rew = self.self_model(batch)
-        return batch.rew + self.get_beta() * i_rew
+        batch.rew += self.get_beta() * i_rew
 
     def process_fn(
         self,

912f29dd718ae0989bdc4142b0df831c7bad1717
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-09-04 19:59:08 +0200

    added goal-based variants of actor and critic networks

--- networks/actor_critic.py
+++ networks/actor_critic.py
@@ -1,11 +1,12 @@
 from typing import Dict, Optional
+from tianshou.data.batch import BatchProtocol, TArr
+from .observation_net import NetHackObsNet
+from gymnasium import Space
+
+# TODO typing imports should be first in ALL files
 
 from torch import nn
 import torch
-
-from gymnasium import Space
-
-from .observation_net import NetHackObsNet
 
 
 class SimpleNetHackActor(nn.Module):
@@ -18,12 +19,40 @@ class SimpleNetHackActor(nn.Module):
 
     def forward(
         self,
-        batch_obs: Dict[str, torch.Tensor],
+        # TODO some other types need changing too...
+        batch_obs: TArr | BatchProtocol,
         state: Optional[torch.Tensor] = None,
         info: Dict = {},
     ):
         obs_out = self.obs_net(batch_obs)
         logits = self.final_layer(obs_out)
+        return logits, state
+
+
+class GoalBasedNetHackActor(nn.Module):
+    def __init__(self, obs_goal_dim: int, action_space: Space):
+        super().__init__()
+
+        self.n_actions = action_space.n
+
+        # we use a two-stream architecture (https://proceedings.mlr.press/v37/schaul15.html)
+        hidden_dim = obs_goal_dim // 3  # obs dim == goal dim
+        # mu == micro
+        self.obs_munet = nn.Sequential(nn.Linear(obs_goal_dim, hidden_dim), nn.ReLU())
+        self.goal_munet = nn.Sequential(nn.Linear(obs_goal_dim, hidden_dim), nn.ReLU())
+
+        self.final_layer = nn.Linear(hidden_dim + hidden_dim, self.n_actions)
+
+    def forward(
+        self,
+        batch_latent_obs: torch.Tensor,
+        batch_latent_goal: torch.Tensor,
+        state: Optional[torch.Tensor] = None,
+        info: Dict = {},
+    ):
+        obss = self.obs_munet(batch_latent_obs)
+        goals = self.goal_munet(batch_latent_goal)
+        logits = self.final_layer(torch.cat((obss, goals), dim=1))
         return logits, state
 
 
@@ -42,4 +71,27 @@ class SimpleNetHackCritic(nn.Module):
     ):
         obs_out = self.obs_net(batch_obs)
         v_s = self.final_layer(obs_out)
+        return v_s
+
+
+class GoalBasedNetHackCritic(nn.Module):
+    def __init__(self, obs_goal_dim: int):
+        super().__init__()
+
+        hidden_dim = obs_goal_dim // 3
+        self.obs_munet = nn.Sequential(nn.Linear(obs_goal_dim, hidden_dim), nn.ReLU())
+        self.goal_munet = nn.Sequential(nn.Linear(obs_goal_dim, hidden_dim), nn.ReLU())
+
+        self.final_layer = nn.Linear(hidden_dim + hidden_dim, 1)
+
+    def forward(
+        self,
+        batch_latent_obs: torch.Tensor,
+        batch_latent_goal: torch.Tensor,
+        state: Optional[torch.Tensor] = None,
+        info: Dict = {},
+    ):
+        obss = self.obs_munet(batch_latent_obs)
+        goals = self.goal_munet(batch_latent_goal)
+        v_s = self.final_layer(torch.cat((obss, goals), dim=1))
         return v_s

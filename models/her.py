e0a4852b61c418aa91607be6e1003df6af8878fd
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-09-12 20:10:47 +0200

    added initial HER code

--- models/her.py
+++ models/her.py
@@ -1,11 +1,9 @@
-from tianshou.data.batch import BatchProtocol
 from core.types import GoalReplayBufferProtocol
 import numpy as np
+import torch
 
-from tianshou.data.buffer.her import HERReplayBuffer
 
-
-class HER(HERReplayBuffer):
+class HER:
     """An implementation of Hindsight Experience Replay (https://arxiv.org/abs/1707.01495).
 
     This version doesn't necessitate goal-based environments, as do current implementations in Tianshou (https://tianshou.org/en/v1.1.0/03_api/data/buffer/her.html#tianshou.data.buffer.her.HERReplayBuffer) and Stable Baselines (https://stable-baselines3.readthedocs.io/en/master/modules/her.html).
@@ -18,37 +16,35 @@ class HER(HERReplayBuffer):
         horizon: int,
         future_k: float = 8.0,
     ) -> None:
-        super().__init__(
-            size=buffer.maxsize,
-            # we perform the reward computation directly in this class
-            compute_reward_fn=None,
-            horizon=horizon,
-            future_k=future_k,
-        )
+        self.buf = buffer
+        self.horizon = horizon
+        self.future_p = 1 - 1 / future_k
+        self.future_k = future_k
 
+    # TODO turn this into a function => I only care about behaviour, not attributes! (i.e., I only need it to calculate the rewards derived from the hindsight replay, that's it)
+    # TODO HER is intrinsic, and we only care about the reward coming form the hindsight replay => no need to manage state, we can keep things pure
     def rewrite_transitions(self, indices: np.ndarray) -> None:
-        """Re-write the goal of some sampled transitions' episodes according to HER."""
         if indices.size == 0:
             return
 
         # Sort self.indices keeping chronological order
-        indices[indices < self._index] += self.maxsize
+        indices[indices < self.buf._index] += self.buf.maxsize
         indices = np.sort(indices)
-        indices[indices >= self.maxsize] -= self.maxsize
+        indices[indices >= self.buf.maxsize] -= self.buf.maxsize
 
         # Construct episode trajectories
         indices = [indices]
         for _ in range(self.horizon - 1):
-            indices.append(self.next(indices[-1]))
+            indices.append(self.buf.next(indices[-1]))
         indices = np.stack(indices)
 
         # Calculate future timestep to use
         current = indices[0]
         terminal = indices[-1]
-        episodes_len = (terminal - current + self.maxsize) % self.maxsize
+        episodes_len = (terminal - current + self.buf.maxsize) % self.buf.maxsize
         future_offset = np.random.uniform(size=len(indices[0])) * episodes_len
         future_offset = np.round(future_offset).astype(int)
-        future_t = (current + future_offset) % self.maxsize
+        future_t = (current + future_offset) % self.buf.maxsize
 
         # Compute indices
         #   open indices are used to find longest, unique trajectories among
@@ -66,57 +62,66 @@ class HER(HERReplayBuffer):
             replace=False,
         )
 
-        # Cache original meta
-        self._altered_indices = unique_ep_indices.copy()
-        self._original_meta = self._meta[self._altered_indices].copy()
-
-        # Copy original obs, ep_rew (and obs_next), and obs of future time step
-        ep_obs = self[unique_ep_indices].obs
-        # to satisfy mypy
-        ep_rew = self[unique_ep_indices].rew
-        if self._save_obs_next:
+        # copy original obs, ep_rew, obs_next (if available), and obs at future time step
+        ep_obs = self.buf[unique_ep_indices].obs
+        ep_rew = self.buf[unique_ep_indices].rew
+        if self.buf._save_obs_next:
+            # if we have an obs_next, use it
             ep_obs_next = self[unique_ep_indices].obs_next
             future_obs = self[future_t[unique_ep_close_indices]].obs_next
         else:
-            future_obs = self[self.next(future_t[unique_ep_close_indices])].obs
+            # else, construct the future_obs from the current one
+            future_obs = self.buf[self.buf.next(future_t[unique_ep_close_indices])].obs
 
-        # Re-assign goals and rewards via broadcast assignment
-        # TODO this should be for desired_goal
-        ep_obs[:, her_ep_indices] = future_obs[None, her_ep_indices]
-        if self._save_obs_next:
-            # TODO desired_goal on left, achieved_goal on right
-            ep_obs_next[:, her_ep_indices] = future_obs[
-                None,
-                her_ep_indices,
-            ]
-            ep_rew[:, her_ep_indices] = self._compute_reward(ep_obs_next)[
-                :, her_ep_indices
-            ]
+        # re-assign goals and rewards
+        # all the desired goals are latent_goals, the achieved goals are computed with a forward pass of the observation net
+        # get_latent_goal will basically pass the current observation through an obs_net
+        self.buf.latent_goal[:, her_ep_indices] = get_achieved_goal(
+            future_obs[None, her_ep_indices]
+        )
+        if self.buf._save_obs_next:
+            # if _save_obs_next is True, then there is a latent_goal_next (check core/buffer.py)
+            self.buf.latent_goal_next[:, her_ep_indices] = get_achieved_goal(
+                future_obs[
+                    None,
+                    her_ep_indices,
+                ]
+            )
+            ep_rew[:, her_ep_indices] = self._compute_reward(
+                self.buf.latent_goal_next, get_achieved_goal(future_obs)
+            )[:, her_ep_indices]
         else:
+            # TODO
             tmp_ep_obs_next = self[self.next(unique_ep_indices)].obs
             ep_rew[:, her_ep_indices] = self._compute_reward(tmp_ep_obs_next)[
                 :, her_ep_indices
             ]
 
         # Sanity check
-        # TODO this should be checked for desired and achieved goals
         assert ep_obs.shape[:2] == unique_ep_indices.shape
         assert ep_obs.shape[:2] == unique_ep_indices.shape
         assert ep_rew.shape == unique_ep_indices.shape
 
-        # Re-write meta
-        self._meta.obs[unique_ep_indices] = ep_obs
-        if self._save_obs_next:
-            self._meta.obs_next[unique_ep_indices] = ep_obs_next  # type: ignore
-        self._meta.rew[unique_ep_indices] = ep_rew.astype(np.float32)
+        return ep_rew.astype(np.float32)
 
-    # TODO this should get desired and achieved goals as input
-    def _compute_reward(self, obs: BatchProtocol, lead_dims: int = 2) -> np.ndarray:
-        lead_shape = obs.shape[:lead_dims]
-        # TODO the two statements below should operate on the goals, not on the observations
-        # desired_goal = obs.reshape(-1, *obs[lead_dims:])
-        # achieved_goal = obs.reshape(-1, *obs[lead_dims:])
-        desired_goal = obs.reshape(-1, obs.shape[-1])
-        achieved_goal = obs.reshape(-1, obs.shape[-1])
-        rewards = self.compute_reward_fn(achieved_goal, desired_goal)
-        return rewards.reshape(*lead_shape)
+    # TODO are the types correct?
+    def _compute_reward(
+        self,
+        desired_goal: torch.Tensor,
+        achieved_goal: torch.Tensor,
+        epsilon: float = 0.001,
+    ) -> np.ndarray:
+        """Computes the rewards for achieving goals.
+
+        It is implemented exactly as specified in the paper, so using the L1 norm and a reward of 0 if an agent reaches the goal and -1 otherwise.
+        """
+        distances = torch.sum(torch.abs(desired_goal - achieved_goal), dim=1)
+        return (distances <= epsilon).float() - 1
+
+
+# def compute_reward_fn(
+#     achieved_goal: np.ndarray, desired_goal: np.ndarray
+# ) -> np.ndarray:
+#     num_flattened_lead_dims = achieved_goal.shape[0]
+#     rewards = np.ones(num_flattened_lead_dims)
+#     return rewards

db2bbf96a9ae0070e038d0b17f1983b01564287a
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-10-02 12:51:12 +0200

    added device support and fixed API compatibility

--- networks/discrete_net.py
+++ networks/discrete_net.py
@@ -1,13 +1,22 @@
 import torch
 import torch.nn as nn
+import gymnasium as gym
 
 
 class DiscreteObsNet(nn.Module):
-    """A simple and flexible observation network for a Discrete observation spaces."""
+    """A simple and flexible observation network for Discrete observation spaces with device support."""
 
-    def __init__(self, hidden_sizes: list[int], o_dim: int):
+    def __init__(
+        self,
+        # for API compatibility
+        observation_space: gym.Space,
+        hidden_sizes: list[int],
+        o_dim: int,
+        device: torch.device = torch.device("cpu"),
+    ):
         super().__init__()
         self.o_dim = o_dim
+        self.device = device
 
         layers = []
         in_size = 1
@@ -16,17 +25,19 @@ class DiscreteObsNet(nn.Module):
             layers.append(nn.ReLU())
             in_size = hidden_size
         layers.append(nn.Linear(in_size, self.o_dim))
-        self.fc = nn.Sequential(*layers)
+        self.fc = nn.Sequential(*layers).to(device)
 
     def forward(self, x):
         x = x["obs"]
-        x = torch.as_tensor(x, dtype=torch.float32)
-
+        x = torch.as_tensor(x, dtype=torch.float32, device=self.device)
         if x.dim() == 0:
             # we get a single scalar input
             x = x.view(1, 1)
         elif x.dim() == 1:
             # we get a batch as input
             x = x.view(-1, 1)
-
         return self.fc(x)
+
+    def to(self, device):
+        self.device = device
+        return super().to(device)

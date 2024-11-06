f3cc4b5694fbbe421896bbb100404e33e2060e6b
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-11-06 12:59:21 +0100

    fixed the code to match the interface provided by HER

--- intrinsic/zero_her.py
+++ intrinsic/zero_her.py
@@ -1,5 +1,4 @@
 from core.types import GoalReplayBufferProtocol
-from tianshou.data import Batch
 import numpy as np
 
 from torch import nn
@@ -10,7 +9,7 @@ from .her import HER
 class ZeroHER(HER):
     """A HER wrapper that does nothing.
 
-    This class overrides the methods of HER to perform no operations, effectively disabling the HER functionality while maintaining the same interface.
+    This class overrides the HER's methods to perform no operations, effectively disabling the HER functionality while maintaining the same interface.
 
     (Alhough a more representative name for it could be NoOpHER, we kept the name similar to ZeroICM, seeing as the purpose of this class is exacly the same: ablation studies.)
     """
@@ -25,19 +24,5 @@ class ZeroHER(HER):
     ) -> None:
         super().__init__(obs_net, buffer, horizon, future_k, epsilon)
 
-    def get_future_observation_(self, indices: np.ndarray) -> Batch:
-        # return the next observations without any modifications
-        return self.buf[indices].obs_next
-
-    def rewrite_transitions_(self, future_achieved_goal: np.ndarray) -> None:
+    def rewrite_rewards_(self, indices: np.ndarray) -> None:
         pass
-
-    def _compute_reward(
-        self,
-        desired_goal: np.ndarray,
-        achieved_goal: np.ndarray,
-        lead_dims: int = 2,
-    ) -> np.ndarray:
-        # return zero rewards
-        lead_shape = desired_goal.shape[:lead_dims]
-        return np.zeros(lead_shape, dtype=np.float32)

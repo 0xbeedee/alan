d2ab6e23f8dc23c5317430c04eb7a211ecc57245
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-11-07 17:32:29 +0100

    added type annotations to the Crop class

--- networks/utils.py
+++ networks/utils.py
@@ -19,7 +19,7 @@ def reparameterise(
 class Crop(nn.Module):
     """Helper class to provide the agent with an egocentric representation of NetHack."""
 
-    def __init__(self, height, width, height_target, width_target):
+    def __init__(self, height: int, width: int, height_target: int, width_target: int):
         super(Crop, self).__init__()
         self.width = width
         self.height = height
@@ -36,7 +36,7 @@ class Crop(nn.Module):
         self.register_buffer("width_grid", width_grid.clone())
         self.register_buffer("height_grid", height_grid.clone())
 
-    def forward(self, inputs, coordinates):
+    def forward(self, inputs: torch.Tensor, coordinates: torch.Tensor):
         """Calculates centered crop around given x,y coordinates (usually the agent's position)."""
         assert inputs.shape[1] == self.height
         assert inputs.shape[2] == self.width
@@ -57,13 +57,12 @@ class Crop(nn.Module):
             dim=3,
         )
 
-        # TODO: only cast to int if original tensor was int
         return (
             torch.round(F.grid_sample(inputs, grid, align_corners=True))
             .squeeze(1)
             .long()
         )
 
-    def _step_to_range(self, delta, num_steps):
+    def _step_to_range(self, delta: float, num_steps: int):
         """Range of `num_steps` integers with distance `delta` centered around zero."""
         return delta * torch.arange(-num_steps // 2, num_steps // 2)

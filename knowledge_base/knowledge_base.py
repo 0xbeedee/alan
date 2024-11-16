037aaa4c180234f7c2fff79c6711f51dd332acdf
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-11-16 16:29:52 +0100

    updated the adding and sampling from the knowledge base

--- knowledge_base/knowledge_base.py
+++ knowledge_base/knowledge_base.py
@@ -1,8 +1,8 @@
-from typing import Any
-from core.types import GoalBatchProtocol
+from typing import Any, cast
+from core.types import KBBatchProtocol
 
 from tianshou.data import ReplayBuffer, ReplayBufferManager, Batch
-from tianshou.data.batch import alloc_by_keys_diff
+from tianshou.data.batch import alloc_by_keys_diff, create_value
 import numpy as np
 
 
@@ -13,8 +13,8 @@ class KnowledgeBase(ReplayBuffer):
     """
 
     # store the an (o_t, a_t, r_t) tuple with an additional initial_obs entry to keep track of the beginning of a trajectory, and a final_obs entry to keep track of the end of said trajectory
-    _reserved_keys = ("obs", "act", "rew", "init_obs")
-    _input_keys = ("obs", "act", "rew", "init_obs")
+    _reserved_keys = ("obs", "act", "rew", "init_obs", "traj_id")
+    _input_keys = ("obs", "act", "rew", "init_obs", "traj_id")
 
     def __init__(
         self,
@@ -31,58 +31,73 @@ class KnowledgeBase(ReplayBuffer):
 
     def add(
         self,
-        batch: GoalBatchProtocol,
+        batch: KBBatchProtocol,
         buffer_ids: np.ndarray | list[int] | None = None,
     ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Adds a batch of data into replay buffer."""
-        # TODO
-        # extract obs, act, rew from batch
-        # assign to each triplet the correct initial_obs
-        #   if the buffer is empty => the initial obs matches obs
-        #   else ...
-
         # preprocess batch
         new_batch = Batch()
         for key in batch.get_keys():
             new_batch.__dict__[key] = batch[key]
         batch = new_batch
-        batch.__dict__["done"] = np.logical_or(batch.terminated, batch.truncated)
-        assert {"obs", "act", "rew", "terminated", "truncated", "done"}.issubset(
+
+        assert {"obs", "act", "rew", "init_obs", "traj_id"}.issubset(
             batch.get_keys(),
-        )  # important to do after preprocess batch
+        )
+
         stacked_batch = buffer_ids is not None
         if stacked_batch:
             assert len(batch) == 1
         if self._save_only_last_obs:
             batch.obs = batch.obs[:, -1] if stacked_batch else batch.obs[-1]
-        if not self._save_obs_next:
-            batch.pop("obs_next", None)
-        elif self._save_only_last_obs:
-            batch.obs_next = (
-                batch.obs_next[:, -1] if stacked_batch else batch.obs_next[-1]
-            )
+
         # get ptr
-        if stacked_batch:
-            rew, done = batch.rew[0], batch.done[0]
-        else:
-            rew, done = batch.rew, batch.done
+        rew = batch.rew[0] if stacked_batch else batch.rew
         ptr, ep_rew, ep_len, ep_idx = (
-            np.array([x]) for x in self._add_index(rew, done)
+            np.array([x]) for x in self._add_index(rew, done=False)
         )
         try:
             self._meta[ptr] = batch
         except ValueError:
             stack = not stacked_batch
-            batch.rew = batch.rew.astype(float)
-            batch.done = batch.done.astype(bool)
-            batch.terminated = batch.terminated.astype(bool)
-            batch.truncated = batch.truncated.astype(bool)
+            batch.rew = batch.rew.astype(np.float32)
+            batch.init_obs = batch.init_obs
+            batch.traj_id = batch.traj_id.astype(int)
             if len(self._meta.get_keys()) == 0:
                 self._meta = create_value(batch, self.maxsize, stack)  # type: ignore
             else:  # dynamic key pops up in batch
                 alloc_by_keys_diff(self._meta, batch, self.maxsize, stack)
             self._meta[ptr] = batch
         return ptr, ep_rew, ep_len, ep_idx
+
+    def __getitem__(
+        self, index: slice | int | list[int] | np.ndarray
+    ) -> KBBatchProtocol:
+        if isinstance(index, slice):  # change slice to np array
+            indices = (
+                self.sample_indices(0)
+                if index == slice(None)
+                else self._indices[: len(self)][index]
+            )
+        else:
+            indices = index  # type: ignore
+
+        # raise KeyError first instead of AttributeError,
+        # to support np.array([ReplayBuffer()])
+        obs = self.get(indices, "obs")
+
+        batch_dict = {
+            "obs": obs,
+            "act": self.act[indices],
+            "rew": self.rew[indices],
+            "init_obs": self.init_obs[indices],
+            "traj_id": self.traj_id[indices],
+        }
+
+        for key in self._meta.__dict__:
+            if key not in self._input_keys:
+                batch_dict[key] = self._meta[key][indices]
+        return cast(KBBatchProtocol, Batch(batch_dict))
 
 
 class KnowledgeBaseManager(KnowledgeBase, ReplayBufferManager):

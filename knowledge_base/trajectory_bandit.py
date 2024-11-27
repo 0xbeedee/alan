ddb154d6933a65a2ae4e943fceb20151771930df
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-11-27 16:39:29 +0100

    updated the bandit

--- knowledge_base/trajectory_bandit.py
+++ knowledge_base/trajectory_bandit.py
@@ -1,43 +1,77 @@
-from typing import List
-from core.types import KBBatchProtocol
+from typing import List, Optional
 
 import torch
+import numpy as np
 
-from .knowledge_base import KnowledgeBase
+from core.types import KBBatchProtocol
 from .utils import is_similar
 
 
 class TrajectoryBandit:
     """A bandit which selects which trajectory from the knowledge base the agent should use."""
 
-    # TODO
-
-    def __init__(self, knowledge_base: KnowledgeBase):
+    def __init__(self, knowledge_base: "KnowledgeBase"):  # type: ignore
         self.knowledge_base = knowledge_base
-        self.arms = {}  # map trajectory indices to bandit values
+        self.arms = {}  # {traj_id: Arm}
 
-    def select_trajectory(self, init_latent_obs: torch.Tensor) -> List[KBBatchProtocol]:
+    def select_trajectory(
+        self, init_latent_obs: torch.Tensor
+    ) -> Optional[KBBatchProtocol]:
         """Extracts a subset of candidate trajectories from the knowledge base."""
         trajectories = self.knowledge_base.get_all_trajectories()
-        # find matching trajectories
-        matching_indices = []
-        # TODO this n^2 nesting is not too pleasant...
-        for idx, traj_buffers in enumerate(trajectories):
-            for traj in traj_buffers:
+
+        matching_arms = []
+        for buffer_traj in trajectories:
+            for traj in buffer_traj:
                 if is_similar(init_latent_obs, traj[0].latent_obs):
-                    matching_indices.append(idx)
-        if not matching_indices:
+                    traj_id = traj[0].traj_id
+                    if traj_id not in self.arms:
+                        self.arms[traj_id] = Arm(traj)
+                    matching_arms.append(self.arms[traj_id])
+        if not matching_arms:
             return None
 
-        # select trajectory using bandit algorithm
-        selected_idx = bandit_algorithm(matching_indices, self.arms)
-        return trajectories[selected_idx]
+        selected_arm = self._UCB1(matching_arms)
+        return selected_arm.trajectory
 
-    def update_bandit(self, trajectory_idx, reward):
-        # TODO update the bandit values based on the received reward
-        pass
+    def _UCB1(self, matching_arms: List[KBBatchProtocol]):
+        """Implements the UCB1 algorithm (https://link.springer.com/article/10.1023/A:1013689704352)."""
+        total_pulls = sum(arm.pulls for arm in matching_arms)
+        ucb_values = [
+            (
+                arm.estimated_value + np.sqrt((2 * np.log10(total_pulls)) / arm.pulls)
+                if arm.pulls > 0
+                else float("inf")
+            )
+            for arm in matching_arms
+        ]
+        selected_arm = matching_arms[np.argmax(ucb_values)]
+        return selected_arm
+
+    def update(self, traj_id: int, reward: int):
+        """Updates the statistics of each arm."""
+        arm = self.arms.get(traj_id)
+        if arm:
+            arm.update(reward)
 
 
-def bandit_algorithm(matching_indices, arms):
-    # TODO bandit algorithm
-    pass
+class Arm:
+    def __init__(
+        self,
+        trajectory: KBBatchProtocol,
+        cumulative_reward: float = 0.0,
+        pulls: int = 0,
+    ):
+        self.trajectory = trajectory
+        self.traj_id = trajectory[0].traj_id  # all traj_ids are the same
+        self.cumulative_reward = cumulative_reward
+        self.pulls = pulls
+
+    def update(self, reward: int):
+        """Updates the statistics associated with each arm."""
+        self.cumulative_reward += reward
+        self.pulls += 1
+
+    @property
+    def estimated_value(self):
+        return self.cumulative_reward / self.pulls if self.pulls > 0 else 0.0

561e155172477a10d3ca13102995cc3d13ff55f7
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-11-29 16:43:40 +0100

    updated similarity computation to maintain batch dimension

--- knowledge_base/utils.py
+++ knowledge_base/utils.py
@@ -3,12 +3,16 @@ import torch
 
 # TODO this is a bit too simplistic...
 def is_similar(
-    latent_obs1: torch.Tensor, latent_obs2: torch.Tensor, thresh: float = 0.90
-) -> bool:
+    batch_latent_obs1: torch.Tensor,
+    batch_latent_obs2: torch.Tensor,
+    thresh: float = 0.90,
+) -> torch.Tensor:
     # TODO cosine similarity (after normalisatin) could also be used here
     # TODO we could use other methods to go from [0, inf) to [0, 1] (e.g., sigmoids, RBF kernels, exp decay)
-    similarity = 1.0 / (1.0 + torch.dist(latent_obs1, latent_obs2, p=2))
+    distance = torch.norm(
+        batch_latent_obs1 - batch_latent_obs2, p=2, dim=1, keepdim=True
+    )  # (B, 1)
+    # TODO I get a very high similarity for some observations, especially in the beginning, why?
+    similarity = 1.0 / (1.0 + distance)
 
-    if similarity > thresh:
-        return True
-    return False
+    return similarity > thresh

ab063270d3f3944cd100de9621b3a1ee3a0bcc45
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-12-03 17:34:00 +0100

    added first functional prototype of the trajectory bandit

--- knowledge_base/trajectory_bandit.py
+++ knowledge_base/trajectory_bandit.py
@@ -1,77 +1,87 @@
-from typing import List, Optional
+from typing import List, Optional, Dict, Tuple
+from core.types import KBBatchProtocol
 
 import torch
 import numpy as np
 
-from core.types import KBBatchProtocol
 from .utils import is_similar
 
 
+class Arm:
+    def __init__(self, trajectory: KBBatchProtocol) -> None:
+        # one arm per trajectory
+        self.trajectory = trajectory
+        # per-trajectory statistics
+        self.cumulative_reward = sum(trajectory.rew)
+        self.pulls = 0
+
+    def update_stats(self, reward: float) -> None:
+        """Updates the statistics of the arm."""
+        self.cumulative_reward += reward
+        self.pulls += 1  # one pull per trajectory update
+
+    @property
+    def estimated_value(self) -> torch.Tensor:
+        """Computes the estimated value of the arm."""
+        return self.cumulative_reward / self.pulls if self.pulls > 0 else 0.0
+
+
 class TrajectoryBandit:
-    """A bandit which selects which trajectory from the knowledge base the agent should use."""
-
-    def __init__(self, knowledge_base: "KnowledgeBase"):  # type: ignore
+    def __init__(self, knowledge_base: "KnowledgeBase"):  # type:ignore
         self.knowledge_base = knowledge_base
-        self.arms = {}  # {traj_id: Arm}
+        self.arms = [{} for _ in range(self.knowledge_base.buffer_num)]
 
-    def select_trajectory(
+    def select_trajectories(
         self, init_latent_obs: torch.Tensor
-    ) -> Optional[KBBatchProtocol]:
-        """Extracts a subset of candidate trajectories from the knowledge base."""
-        trajectories = self.knowledge_base.get_all_trajectories()
+    ) -> Tuple[List[Optional[KBBatchProtocol]], List[int]]:
+        selected_trajectories = []
+        # we need to keep track of the buffers from which we extracted the trajectories
+        selected_buffer_ids = []
+        # TODO this assumes that the traj_id always correspond to the same trajectories over time, but that is only possible with an infinitely sized buffer!
+        # (could i maybe cache the currently saved arms somehow?)
+        for buffer_id in range(self.knowledge_base.buffer_num):
+            matching_arms = []
+            buffer_arms = self.arms[buffer_id]
+            for traj_id in range(self.knowledge_base.n_trajectories):
+                traj = self.knowledge_base.get_single_trajectory(traj_id, buffer_id)
+                if traj is None:
+                    continue
+                # TODO sometimes init_latent_obs does not have correct batch dims (num_ready_envs in the Collector might be the issue)
+                if is_similar(
+                    init_latent_obs[buffer_id].unsqueeze(0),
+                    traj.latent_obs[0].unsqueeze(0),
+                ):
+                    if traj_id not in buffer_arms:
+                        # if traj_id is new, create the entry; it traj_id is already present, overwrite the entry
+                        # TODO doing it this way is somewhat inefficient
+                        buffer_arms[traj_id] = Arm(traj)
+                    matching_arms.append(buffer_arms[traj_id])
+            if matching_arms:
+                # select one arm from each buffer
+                selected_arm = self._UCB1(matching_arms)
+                # TODO technically, we could only save the actions in this array, they are the only ones we need in policy.forward()
+                selected_trajectories.append(selected_arm.trajectory)
+                selected_buffer_ids.append(buffer_id)
 
-        matching_arms = []
-        for buffer_traj in trajectories:
-            for traj in buffer_traj:
-                if is_similar(init_latent_obs, traj[0].latent_obs):
-                    traj_id = traj[0].traj_id
-                    if traj_id not in self.arms:
-                        self.arms[traj_id] = Arm(traj)
-                    matching_arms.append(self.arms[traj_id])
-        if not matching_arms:
-            return None
+        return selected_trajectories, selected_buffer_ids
 
-        selected_arm = self._UCB1(matching_arms)
-        return selected_arm.trajectory
-
-    def _UCB1(self, matching_arms: List[KBBatchProtocol]):
-        """Implements the UCB1 algorithm (https://link.springer.com/article/10.1023/A:1013689704352)."""
+    def _UCB1(self, matching_arms: List[Arm]) -> Arm:
         total_pulls = sum(arm.pulls for arm in matching_arms)
-        ucb_values = [
-            (
-                arm.estimated_value + np.sqrt((2 * np.log10(total_pulls)) / arm.pulls)
-                if arm.pulls > 0
-                else float("inf")
-            )
-            for arm in matching_arms
-        ]
+        ucb_values = []
+        for arm in matching_arms:
+            arm_pulls = arm.pulls
+            arm_estimated_value = arm.estimated_value
+            # add an epsilon to avoid division by 0
+            bonus = np.sqrt((2 * np.log(total_pulls + 1e-5)) / (arm_pulls + 1e-7))
+            ucb_value = arm_estimated_value + bonus
+            ucb_values.append(ucb_value)
         selected_arm = matching_arms[np.argmax(ucb_values)]
         return selected_arm
 
-    def update(self, traj_id: int, reward: int):
-        """Updates the statistics of each arm."""
-        arm = self.arms.get(traj_id)
-        if arm:
-            arm.update(reward)
-
-
-class Arm:
-    def __init__(
-        self,
-        trajectory: KBBatchProtocol,
-        cumulative_reward: float = 0.0,
-        pulls: int = 0,
-    ):
-        self.trajectory = trajectory
-        self.traj_id = trajectory[0].traj_id  # all traj_ids are the same
-        self.cumulative_reward = cumulative_reward
-        self.pulls = pulls
-
-    def update(self, reward: int):
-        """Updates the statistics associated with each arm."""
-        self.cumulative_reward += reward
-        self.pulls += 1
-
-    @property
-    def estimated_value(self):
-        return self.cumulative_reward / self.pulls if self.pulls > 0 else 0.0
+    def update(self, traj_rewards: List[Dict[int, float]]) -> None:
+        for buffer_id, buffer_rewards in enumerate(traj_rewards):
+            buffer_arms = self.arms[buffer_id]
+            for traj_id, reward in buffer_rewards.items():
+                arm = buffer_arms.get(traj_id)
+                if arm:
+                    arm.update_stats(reward)

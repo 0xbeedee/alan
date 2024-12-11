5c320caa9d5f79aec53ddd478712c93815d2ddb2
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2024-12-11 15:25:12 +0100

    modified trajectory selection to obtain more independence from the KB

--- lifelong/trajectory_bandit.py
+++ lifelong/trajectory_bandit.py
@@ -31,7 +31,11 @@ class Arm:
 class TrajectoryBandit:
     def __init__(self, knowledge_base: "KnowledgeBase"):  # type:ignore
         self.knowledge_base = knowledge_base
-        self.arms = [{} for _ in range(self.knowledge_base.buffer_num)]
+        self.global_arm_id = 0
+        # global dict tracking all the available arms with a unique ID
+        self.all_arms = {}  # {arm_id: Arm}
+        # per-buffer mapping from traj_id to global arm_id
+        self.arm_ids_by_buffer = {}  # {buffer_id: {traj_id: arm_id}}
 
     def select_trajectories(
         self, init_env_obs: Batch, ready_env_ids: np.ndarray, obs_net: nn.Module
@@ -45,22 +49,36 @@ class TrajectoryBandit:
         selected_buffer_ids = []
         for buffer_id in ready_env_ids:
             # only update the data for the ready environments (env_id and buffer_id are one-to-one)
+            if buffer_id not in self.arm_ids_by_buffer:
+                self.arm_ids_by_buffer[buffer_id] = {}
+            arm_ids_by_traj = self.arm_ids_by_buffer[buffer_id]
             matching_arms = []
-            buffer_arms = self.arms[buffer_id]
-            for traj_id in range(self.knowledge_base.n_trajectories):
+
+            for traj_id in range(self.knowledge_base.n_trajectories - 1):
+                # only consider complete trajectories
                 traj = self.knowledge_base.get_single_trajectory(traj_id, buffer_id)
                 if traj is None or len(traj) == 0:
                     continue
                 if is_similar(obs_net, init_env_obs[buffer_id], traj.obs[0]):
-                    arm = Arm(traj)
-                    if (
-                        traj_id in buffer_arms
-                        and arm.trajectory == buffer_arms[traj_id].trajectory
-                    ):
-                        continue
-                    # the trajectory is new/different, so overwrite the old data
-                    buffer_arms[traj_id] = arm
-                    matching_arms.append(buffer_arms[traj_id])
+                    # check if it's been assigned a global arm ID
+                    if traj_id in arm_ids_by_traj:
+                        arm_id = arm_ids_by_traj[traj_id]
+                        arm = self.all_arms[arm_id]
+                        if arm.trajectory != traj:
+                            # overwrite in global dict and buffer reference
+                            updated_arm = Arm(traj)
+                            self.all_arms[arm_id] = updated_arm
+                            matching_arms.append(updated_arm)
+                        else:
+                            # same arm as before
+                            matching_arms.append(arm)
+                    else:
+                        # brand new trajectory: assign a global ID and store it
+                        self.global_arm_id += 1
+                        new_arm = Arm(traj)
+                        self.all_arms[self.global_arm_id] = new_arm
+                        arm_ids_by_traj[traj_id] = self.global_arm_id
+                        matching_arms.append(new_arm)
             if matching_arms:
                 # select one arm from each buffer
                 selected_arm = self._UCB1(matching_arms)
@@ -87,7 +105,9 @@ class TrajectoryBandit:
     def update(self, traj_rewards: Dict[Tuple[int, int], float]) -> None:
         """Updates the data associated with each arm based on the chosen trajectories."""
         for (buffer_id, traj_id), reward in traj_rewards.items():
-            buffer_arms = self.arms[buffer_id]
-            arm = buffer_arms.get(traj_id)
-            if arm:
-                arm.update_stats(reward)
+            if buffer_id in self.arm_ids_by_buffer:
+                arm_ids_by_traj = self.arm_ids_by_buffer[buffer_id]
+                if traj_id in arm_ids_by_traj:
+                    arm_id = arm_ids_by_traj[traj_id]
+                    arm = self.all_arms[arm_id]
+                    arm.update_stats(reward)

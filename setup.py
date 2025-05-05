e01578640ee70ffe96ec65ba937f60d929c27905
de-lachende-cavalier <81985269+de-lachende-cavalier@users.noreply.github.com>
2025-05-05 16:12:20 +0200

    udpated the setup spec

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+import os
 from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+with open(os.path.join(this_directory, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+requirements = [req for req in requirements if req and not req.startswith("#")]
 
 setup(
     name="alan",
-    version="0.1",
-    packages=find_packages(),
+    version="0.5.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/de-lachende-cavalier/alan",
+    packages=find_packages(exclude=["*.egg-info", "tests*", "docs*", "build*"]),
+    install_requires=requirements,
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
